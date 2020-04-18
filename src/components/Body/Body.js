d8f53ab9e9e0752a6545a2e5b88a1bee6ee2fc8f
DanielRamirezX <daniel.carlos98@hotmail.com>
2020-04-18 16:28:10 -0500

    Header and body-background

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,9 +5,11 @@ import './Body.scss';
 export default function Body(){
     return(
         <div className="Body" >
-            <button id="Prueba">
-                Bienvenido
-            </button>
+            <div position="relative">
+                <img src="./Img/paisaje.gif" alt="animated" height="100%" width="100%"/>
+                <img src="./Img/Logo.jpg" alt="Logo" height="30%" width="30%" position="absolute"/>
+            </div>
+
         </div>
 
     )
