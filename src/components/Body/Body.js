9d758b24ea75588e835955341877db0ad0b17d88
DanielRamirezX <daniel.carlos98@hotmail.com>
2020-04-18 16:52:41 -0500

    Fixed

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,11 +5,7 @@ import './Body.scss';
 export default function Body(){
     return(
         <div className="Body" >
-            <div position="relative">
-                <img src="./Img/paisaje.gif" alt="animated" height="100%" width="100%"/>
                 <img src="./Img/Logo.jpg" alt="Logo" height="30%" width="30%" position="absolute"/>
-            </div>
-
         </div>
 
     )
