5303314058260dbf5b592a32c047508d7795a371
Quintaneishon <ajitzi.quintana@gmail.com>
2020-04-23 16:35:36 -0500

    change de sql script

--- Db.sql
+++ Db.sql
@@ -1,15 +1,14 @@
-/*create table Tipo_Carrera(
+/*
+create table Tipo_Carrera(
 	Id_tipo tinyint primary key,
 	Nombre varchar(30)
 );
-
 create table Carrera(
 	Id_carrera tinyint Primary key,
 	Nombre varchar(100),
 	Id_tipo tinyint foreign key references Tipo_Carrera(Id_tipo),
 	Imagen varchar(200)
 );
-
 create table Ranking(
 	Id_ranking tinyint Primary key,
 	Id_carrera tinyint Foreign key references Carrera(Id_carrera),
@@ -19,7 +18,6 @@ create table Ranking(
 	Escuela4 varchar(100),
 	Escuela5 varchar(100)
 );
-
 create table Cursos(
 	Id_curso tinyint,
 	Nombre varchar (50),
@@ -28,7 +26,6 @@ create table Cursos(
 	Descripcion text,
 	primary key (Id_curso,Id_carrera)
 );
-
 create table Herramienta(
 	Id_herramienta tinyint,
 	Nombre varchar (50),
@@ -38,12 +35,10 @@ create table Herramienta(
 	Descripcion text,
 	primary key (Id_herramienta,Id_carrera)
 );
-
 create table Temario(
 	Id_temario tinyint primary key,
 	Id_carrera tinyint foreign key references Carrera(Id_carrera)
 );
-
 create table Materia(
 	Id_materia tinyint primary key,
 	Id_temario tinyint foreign key references Temario(Id_temario),
@@ -51,15 +46,18 @@ create table Materia(
 	Descripcion text,
 	Dificultad tinyint
 );
-
 create table Apuntes(
 	Id_apuntes tinyint primary key,
 	Imagen varchar(200),
 	Id_materia tinyint foreign key references Materia(Id_materia)
 );
-*/
-Insert into Materia
-values (0,0,'Crypto','criptografía',5);
 
-Insert into Apuntes
-values (0,'ISO_IEC_38500_2008.pdf',0);
+insert into Tipo_Carrera values(0,'ingenieria');
+insert into Carrera values (0,'sistemas compuitacionales',0,'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT33XwzoKKCgcUT0dnvBjvXuN7NhYvg1fGxhvBouHSUb-Y7gcw4&usqp=CAU');
+insert into Ranking values (0,0,'IPN','UNITEC','UNAM','UVM','TESCO');
+insert into Herramienta values (0,'photomath','https://www.photomath.net/en/',0,'photo','herramienta que te ayuda a escanear problemas matem,aticos y los resuelve');
+insert into Temario values (0,0);
+Insert into Materia values (0,0,'Crypto','criptografÃ­a',5);
+Insert into Apuntes values (0,'ISO_IEC_38500_2008.pdf',0);
+
+*/
