952d02643191d87d9b24b5235fafd3a33b4bbd92
Quintaneishon <ajitzi.quintana@gmail.com>
2020-05-03 15:46:32 -0500

    ready new footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,50 +6,44 @@ import './Footer.scss';
 export default function Footer(){
 
     return(
-        <div className='footer'>
-            <img src='./Img/devcows.png' width='35' height='35' /><br></br><span className='footer__name'>DevCows </span><br></br>
-            <Dev nombre='Ajitzi R. Quintana Ruiz'>
-                <a className='icon' href='https://www.facebook.com/ajitzi.quintana' target='_blank'> 
-                    <FaFacebook />
-                </a>
-                <a className='icon' href='https://github.com/Quintaneishon' target='_blank'> 
-                    <FaGithub />
-                </a>
-                <a className='icon' href='https://quintaneishon.github.io/personal-page/' target='_blank'> 
-                    <FaUserGraduate />
-                </a>
-                <a className='icon' href='https://www.linkedin.com/in/ajitzi-ricardo-quintana-ruiz-385a61138/' target='_blank'> 
-                    <FaLinkedinIn />
-                </a>
-            </Dev>
-            <Dev nombre='Carlos D. Vaca Ramírez'>
-                <a className='icon' href='https://www.facebook.com/naruto5daniel' target='_blank'> 
-                    <FaFacebook />
-                </a>
-                <a className='icon' href='https://github.com/DanielRamirezX' target='_blank'> 
-                    <FaGithub />
-                </a>
-                <a className='icon' href='' target='_blank'> 
-                    <FaUserGraduate />
-                </a>
-                <a className='icon' href='https://www.linkedin.com/in/daniel-vaca-ramirez-797438194/' target='_blank'> 
-                    <FaLinkedinIn />
-                </a>
-                <a className='icon' href='https://twitter.com/DanielVacaR' target='_blank'>
-                    <FaTwitter />
-                </a>
-            </Dev>
-        </div>
-    )
-}
-
-function Dev(props){
-    const {nombre} = props
-
-    return(
-        <div className='dev'>
-            <span className='dev__name'>{nombre}</span><br></br>
-            {props.children}
-        </div>
+        <footer className='footer'>
+            <div className='row'>
+                <div className='col-1-of-4'>
+                    <div className='footer__logo-box'>
+                        <img src='./Img/devcows.png' alt='Full logo' className='footer__logo' />
+                    </div>
+                </div>
+                <div className='col-1-of-4'>
+                    <div className='footer__navigation'>
+                        <div className='footer__name u-margin-bottom-small'>Ajitzi R. Quintana Ruiz</div>
+                        <ul className='footer__list u-margin-bottom-medium'>
+                            <li className='footer__item'><a href='https://www.facebook.com/ajitzi.quintana' target='_blank' className='footer__link'><FaFacebook /></a></li>
+                            <li className='footer__item'><a href='https://github.com/Quintaneishon' target='_blank' className='footer__link'><FaGithub /></a></li>
+                            <li className='footer__item'><a href='https://quintaneishon.github.io/personal-page/' target='_blank' className='footer__link'><FaUserGraduate /></a></li>
+                            <li className='footer__item'><a href='https://www.linkedin.com/in/ajitzi-ricardo-quintana-ruiz-385a61138/' target='_blank' className='footer__link'><FaLinkedinIn /></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='col-1-of-4'>
+                <div className='footer__navigation'>
+                        <div className='footer__name u-margin-bottom-small'>Naturo 5 Daniel</div>
+                        <ul className='footer__list'>
+                            <li className='footer__item'><a href='https://www.facebook.com/naruto5daniel' target='_blank' className='footer__link'><FaFacebook /></a></li>
+                            <li className='footer__item'><a href='https://github.com/DanielRamirezX' target='_blank' className='footer__link'><FaGithub /></a></li>
+                            <li className='footer__item'><a href='https://www.linkedin.com/in/daniel-vaca-ramirez-797438194/' target='_blank' className='footer__link'><FaUserGraduate /></a></li>
+                            <li className='footer__item'><a href='https://twitter.com/DanielVacaR' target='_blank' className='footer__link'><FaLinkedinIn /></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='col-1-of-4'>
+                    <p className='footer__copyright'>
+                        Esta página fue creada para fines didacticos, de aprendizaje y apoyo a la educación. Se usaron diferentes herramientas encontradas en este <a href='http://codingheroes.io/resources/' target='_blank' className='footer__link'>sitio</a> El uso de esta pagina es abierto para todo el que desee copiar el diseño. Favor de dar los respectivos creditos a los desarrolladores aqui mencionados.
+                    </p>
+                </div>
+            </div>
+            <div className='text-align footer__footer'>
+                Hecho en México. &copy; 2020 DevCows, Inc. &nbsp;&nbsp;Terms of Service &nbsp;&nbsp;Privacy Policy &nbsp;&nbsp;Legal
+            </div>
+        </footer>
     )
 }
