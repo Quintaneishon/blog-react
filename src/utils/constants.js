b8b92a97dfdcdd04743a96b22ef9a129b34f63d1
DanielRamirezX <daniel.carlos98@hotmail.com>
2020-05-04 16:53:40 -0500

    little changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 
-export const urlApiCarreras = "http://localhost:3001/";
-export const urlApiCarrera = "http://localhost:3001/carrera/";
+export const urlApiCarreras = "http://192.168.3.3:3001/";
+export const urlApiCarrera = "http://192.168.3.3:3001/carrera/";
