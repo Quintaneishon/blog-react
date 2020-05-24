6e2e3188eef0720a024a80ea3270526fdd9f37a3
DanielRamirezX <daniel.carlos98@hotmail.com>
2020-05-23 21:38:02 -0500

    Sidebar design finished

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -6,12 +6,12 @@ export default function SideBar(){
     return(
         <div className='side-bar'>
             <h2 id="title">Menú</h2>
-            <div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-tools"></use></svg><a>Herramientas/Cursos</a></div>
-            <div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-book"></use></svg><a>Cursos</a></div>
-            <div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-pencil"></use></svg><a>Apuntes</a></div>
-            <div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-browser"></use></svg><a>Temario General</a></div>
-            <div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-quote"></use></svg><a>Foro</a></div>
-            <div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-stats-bars"></use></svg><a>Ranking México</a></div>
+            <div id="anim"><div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-tools"></use></svg><a>Herramientas/Cursos</a></div></div>
+            <div id="anim"><div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-book"></use></svg><a>Cursos</a></div></div>
+            <div id="anim"><div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-pencil"></use></svg><a>Apuntes</a></div></div>
+            <div id="anim"><div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-browser"></use></svg><a>Temario General</a></div></div>
+            <div id="anim"><div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-quote"></use></svg><a>Foro</a></div></div>
+            <div id="anim"><div><svg id="icon"><use xlinkHref="../Icons/sprite.svg#icon-stats-bars"></use></svg><a>Ranking México</a></div></div>
         </div>
     )
 }
