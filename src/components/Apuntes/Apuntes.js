405a98773e368fc838cc524d6fba128ed72d4669
Quintaneishon <ajitzi.quintana@gmail.com>
2020-05-30 19:32:42 -0500

    first look apuntes

--- src/components/Apuntes/Apuntes.js
+++ src/components/Apuntes/Apuntes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Apuntes.scss';
 
@@ -24,29 +24,50 @@ export default function Apuntes(props){
     return(
         <div className='apuntes__container'>
             {materias.map((apunte,index) =>(
-                <div className='apuntes__item' key={index}>
-                    <div className='apuntes__nombre apuntes__nombre-activo'>
-                        {apunte.titulo}
-                        <svg className='apuntes__icon'>
-                            <use xlinkHref='../Icons/sprite.svg#icon-chevron-thin-down'/>
-                        </svg>
-                    </div>
-                    <Apunte nombres={apunte.nombre} imagenes={apunte.imagen} />
-                </div>
+                <Item apunte={apunte} key={index} />
             ))}
         </div>
     )
 }
 
+function Item(props){
+    const{apunte} = props;
+    const[active,setActive] = useState(false);
+    console.log(active)
+    return (
+        <div className='apuntes__item'  >
+            <div className='apuntes__nombre' onClick={()=> setActive(!active)}>
+                <div className='apuntes__titulo'>{apunte.titulo}</div>
+                <div className='apuntes__dificultad'>
+                    <svg className='apuntes__alert'>
+                    <use xlinkHref='../Icons/sprite.svg#icon-notification'/>
+                    </svg>
+                    <div className='apuntes__rango'>{apunte.dificultad}/<span>5</span></div>
+                </div>
+                <div className='apuntes__button' >
+                    <svg className='apuntes__flecha'>
+                        {active
+                            ? <use xlinkHref='../Icons/sprite.svg#icon-chevron-thin-down'/>
+                            : <use xlinkHref='../Icons/sprite.svg#icon-chevron-thin-up'/>}
+                    </svg>
+                </div>
+            </div>
+            <Apunte nombres={apunte.nombre} imagenes={apunte.imagen} active={active} />
+        </div>
+    )
+}
+
 function Apunte(props){
-    const {nombres,imagenes} = props;
+    const {nombres,imagenes,active} = props;
     let code = [];
     for (let i=0;i<nombres.length;i++){
-        code.push(<li className='apuntes__lista-item' key={i}>{`${nombres[i]} - ${imagenes[i]}`}</li>);
+        code.push(<li className='apuntes__lista-item' key={i} onClick={() => window.open(`/apuntes/${imagenes[i]}`) } >
+            <div className='apuntes__lista__name' >{nombres[i]}</div>
+            </li>);
     }
 
     return(
-        <ul className='apuntes__lista'>
+        <ul className={'apuntes__lista' + (active ? ' apuntes__lista--active' : '')}>
             {code}
         </ul>
     )
